61b90db80ffcc49a9c9ee56a840954ef1d35c53c
FranciscoM <fmahecha@consorcioqutubminar.com>
2025-02-14 11:01:36 -0500

    Correccion diseño inicio,vista proyecto, equipo,software

---
+++ assets/js/contador.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll('.counter');
+
+    const startCounter = (counter) => {
+        const target = +counter.getAttribute("data-target");
+        let count = 0;
+        const increment = target / 150; 
+
+        const updateCounter = () => {
+            count += increment;
+            if (count < target) {
+                counter.innerText = Math.floor(count);
+                requestAnimationFrame(updateCounter);
+            } else {
+                counter.innerText = target; 
+            }
+        };
+
+        updateCounter();
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                startCounter(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    }, { threshold: 0.5 });
+
+    counters.forEach((counter) => observer.observe(counter));
+});
